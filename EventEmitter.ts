49757d5b89dde69628a5e2b9fe6d2c561862469b
Michael Ord <michael.ord@gmail.com>
2019-09-05 13:01:14 +0100

    code updates

---
+++ EventEmitter.ts
@@ -0,0 +1,48 @@
+// https://basarat.gitbooks.io/typescript/docs/tips/typed-event.html
+// https://dev.to/tunaxor/the-beautiful-thing-called-eventemitter-23ei
+
+const EVENTS: any = {};
+
+class EventEmitter {
+	subscribe(event: string, callback: any) {
+		if (!EVENTS[event]) {
+			EVENTS[event] = [];
+		}
+
+		EVENTS[event].push(callback);
+	}
+
+	unsubscribe(name: string, callback = null) {
+		if (!EVENTS[name]) {
+			return;
+		}
+
+		if (callback) {
+			for (let i = 0; i < EVENTS[name].length; i++) {
+				if (EVENTS[name][i] === callback) {
+					EVENTS[name][i] = null;
+
+					return;
+				}
+			}
+
+			return;
+		}
+
+		delete EVENTS[name];
+	}
+
+	dispatch(name: string, ...payload: any) {
+		if (!EVENTS[name]) {
+			return;
+		}
+
+		for (let i = 0; i < EVENTS[name].length; i++) {
+			if (EVENTS[name][i]) {
+				EVENTS[name][i](...payload);
+			}
+		}
+	}
+}
+
+export default new EventEmitter();
